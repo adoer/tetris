7549a90a39d24d4242f0db6afd67c1501dd29c4f
duer <957965700@qq.com>
2023-02-05 23:32:39 +0800

    Modified   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Antd from 'ant-design-vue';
+import { Table, ConfigProvider } from 'ant-design-vue'
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import 'ant-design-vue/dist/antd.css';
@@ -16,8 +16,10 @@ let instance = axios.create({
 });
 
 const app = createApp(App);
-app.use(VueAxios,instance);
-app.use(Antd).mount('#app');
+app.use(VueAxios, instance);
+app.use(Table)
+app.use(ConfigProvider)
+app.mount('#app');
 
 app.config.productionTip = false;
 
